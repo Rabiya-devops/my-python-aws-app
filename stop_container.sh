c4a3aa9551f69681de3f8bc49bb5c61098e202cd
Rabiya Akram <rabiyaakram57@gmail.com>
2025-10-14 16:51:22 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Stop all containers for this app
-CONTAINER_ID=$(docker ps -aq --filter "ancestor=my-python-app:latest")
+set -e
 
-if [ ! -z "$CONTAINER_ID" ]; then
-    echo "Stopping container $CONTAINER_ID"
-    docker stop $CONTAINER_ID
-    docker rm $CONTAINER_ID
-else
-    echo "No running container to stop."
-fi
+# Stop the running container (if any)
+echo "Hi"
