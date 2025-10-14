f80fe323f5428402bcc84c1f7f1ded81d0172f7b
Rabiya Akram <rabiyaakram57@gmail.com>
2025-10-14 16:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-# app.py
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Example: Print a custom message and the container hostname
-    return f"<h1>Hello from a Docker Container!</h1><p>Container ID: {os.uname()[1]}</p>"
+    return 'Hello, world!'
 
 if __name__ == '__main__':
-    # Use 0.0.0.0 to listen on all public IPs
-    # The default port for Flask is 5000, which we will use.
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
